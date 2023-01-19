57bf6cb3a479925fce57fc988513b670f7c64070
whosbax <whosbax@gmail.com>
2023-01-19 16:48:13 +0100

    fix modjs

--- tests/lucid/dist/esm/src/provider/mod.js
+++
@@ -1,3 +0,0 @@
-export * from "./blockfrost.js";
-export * from "./kupmios.js";
-export * from "./emulator.js";
--- tests/lucid/dist/types/src/utils/cost_model.d.ts
+++
@@ -1,5 +0,0 @@
-import { Core } from "../core/mod.js";
-import { CostModels } from "../mod.js";
-import { ProtocolParameters } from "../types/types.js";
-export declare function createCostModels(costModels: CostModels): Core.Costmdls;
-export declare const PROTOCOL_PARAMETERS_DEFAULT: ProtocolParameters;
